491708a2d3581c9fb79e6a49e3369a8d57c65e81
sriker123 <sreekar.spike@gmail.com>
2017-01-05 13:08:02 +0530

    Constructor
    constructor overloading is explained by executing

---
+++ Constructor/src/com/Construcor/Constructor.java
@@ -0,0 +1,34 @@
+package com.Construcor;
+
+public class Constructor
+{
+int i;
+String name;
+int age;
+Constructor(int a, String b, int c)
+{
+	i=a;
+	name = b;
+	age = c;
+
+	System.out.println(i+" "+name+" "+age);
+}
+Constructor(int a, String b)
+{
+	i=a;
+	name=b;
+	System.out.println(i+" "+name);
+}
+
+
+public void Display()
+{
+	// TODO Auto-generated method stub
+	
+
+	
+
+	
+}
+
+}
---
+++ Constructor/src/com/Construcor/Test.java
@@ -0,0 +1,13 @@
+package com.Construcor;
+import com.Construcor.Test;
+
+public class Test {
+public static void main(String args[])
+{
+	Constructor obj = new Constructor(100,"SRIKER");
+	Constructor obj1 = new Constructor(100,"SRIKER",22);
+
+	obj.Display();
+	obj1.Display();
+}
+}
